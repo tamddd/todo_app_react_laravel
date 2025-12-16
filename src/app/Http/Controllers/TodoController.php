6a77a0be1a37eaf01ada5b2a15611a6752250c6a
tamura <daiki1106tamu@gmail.com>
2025-12-16 16:17:36 +0900

    controllerの処理の追加

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $req = $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create($req);
+
+        return response()->json($todo, 201);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+
+        return response()->json(null, 204);
+    }
+}
